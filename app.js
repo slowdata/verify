8e2ccfb04d4e0cc61d0db69e4b6a65535cb7856c
Ricardo.V.Dias <ii0602@iies.seg-social.pt>
2016-12-15 17:51:27 +0000

    Initial commit and some work

---
+++ app.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular.module('TenrinhosApp', [])
+        .controller('TenrinhosController', TenrinhosController);
+
+    
+    function TenrinhosController() {
+        var tenr = this;
+
+        tenr.teste = "Ohh jazussss!!"
+
+    }
+
+
+})();
