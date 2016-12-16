491b0128862775fa35b4f30b380e580635b5410f
slowdata <slowmotion@outlook.com>
2016-12-16 01:44:02 +0000

    feito, parece

--- app.js
+++ app.js
@@ -8,7 +8,23 @@
     function TenrinhosController() {
         var tenr = this;
 
-        tenr.teste = "Ohh jazussss!!"
+        tenr.showNome = false;
+        tenr.showSobrnome = false;
+
+        tenr.name="";
+        tenr.sobrenome="";
+
+        tenr.says = `Os primeiros Bit's que chegam ao CPU do Tenrinhos BOT é de que és uma PROFISSIONAL mas 
+        também chegam Bit's que dizem que és muito MEIGA! mas isto é o BOT a falar...
+        FELIZ NATAL!`
+
+        console.log(tenr.name);
+        tenr.checkName = function() {
+            tenr.showNome = (tenr.name.toLowerCase() === "vera") ? true : false;
+            tenr.showSobrnome = (tenr.sobrenome.toLowerCase() === "jesus") ? true : false
+            
+            tenr.show = tenr.showNome && tenr.showSobrnome;
+        }
 
     }
 
